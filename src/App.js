57d59e753fa4d2c31d1a9372ccf8e758dd6c7213
izabelle wilding <izabellewilding@gmail.com>
2020-11-17 06:43:10 +0000

    React Query API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useQuery, QueryCache, ReactQueryCacheProvider } from "react-query";
+import Loading from "./components/Loading";
+
+const queryChache = new QueryCache();
 
 function App() {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("https://public.ecologi.com/trees").then((res) => res.json())
+  );
+
+  if (isLoading) return isLoading;
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactQueryCacheProvider queryCache={queryChache}>
+      <Loading loading={isLoading} />
+      <div className="App">
+        <header></header>
+      </div>
+    </ReactQueryCacheProvider>
   );
 }
 
