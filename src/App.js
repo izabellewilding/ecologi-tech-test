d8b410c4024f1f16955da4e52f23f6fc8bf84647
izabelle wilding <izabellewilding@gmail.com>
2020-11-17 09:09:05 +0000

    Comments & Final bits

--- src/App.js
+++ src/App.js
@@ -42,23 +42,27 @@ function App() {
   if (isLoading) return <Loading loading={isLoading} />;
   if (error) return "An error has occurred: " + error.message;
 
-  //order purchases array by createdAt date: https://stackoverflow.com/questions/8837454/sort-array-of-objects-by-single-key-with-date-value
+  //order purchases array by createdAt date:
+  //resource: https://stackoverflow.com/questions/8837454/sort-array-of-objects-by-single-key-with-date-value
   const orderedByDay = data.data.sort(function (a, b) {
     return new Date(a.createdAt) - new Date(b.createdAt);
   });
 
   //groups an array of ordered purchases by day (createdAt)
   //returns an object with arrays of objects
+  //resource: https://lodash.com/docs/4.17.15#groupBy
   const groupedByDay = groupBy(orderedByDay, (item) => {
     return item.createdAt.split("T")[0];
   });
 
   //add total purchases per day and return new object from the Map
+  //resources: https://dev.to/attacomsian/object-entries-and-object-values-methods-in-javascript-3l8c, https://stackoverflow.com/questions/47841899/js-map-return-object
   const totalPurchasesPerDay = Object.entries(groupedByDay).map(
     ([date, purchases]) => {
       const dailyPurchaseObj = {
         date: date,
         trees: purchases
+          //filter only the number dates - data was being weird, assuming there's a string in it
           .filter((purchases) => typeof purchases.value === "number")
           .reduce((acc, purchases) => acc + purchases.value, 0),
       };
@@ -66,6 +70,7 @@ function App() {
     }
   );
 
+  //I WOULD TRY AND COMBINE THIS WITH THE CODE ABOVE WITH MORE TIME
   //group the array of ordered purchases by the month and year
   const groupedByYearMonth = groupBy(orderedByDay, (item) => {
     return item.createdAt.substring(0, 7);
