348f3aa260b890cd796118238a8dd9e98b8e8d49
izabelle wilding <izabellewilding@gmail.com>
2020-11-17 06:52:25 +0000

    Initialise Chart Component

---
+++ src/components/Chart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+export default function Chart({ treesPurchases }) {
+  return <lineChart></lineChart>;
+}
